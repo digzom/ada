0f20cbf177c71803dc1bc29508f08a6ceb27c085
digzom <dicksonmelo@gmail.com>
2023-11-10 22:58:10 -0300

    chore: new list methods

---
+++ python/metodos_de_lista.py
@@ -0,0 +1,17 @@
+# tem um monte kk
+
+lista = [1, 2, 3, 4, 5, 6]
+
+lista.append(7) # insere no final
+
+print(lista)
+
+lista.insert(4, 10) # vai no índice 4 e adiciona o 10 lá, "empurrando" os outros para frente
+
+print(lista)
+
+lista2 = [11, 22, 33]
+
+lista.extend(lista2) # tipo um merge
+
+print(lista)
